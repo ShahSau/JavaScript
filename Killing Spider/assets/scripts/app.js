b83d240274896c891911949af63701ee18c0de4a
ShahSau <shahriarksaurov@gmail.com>
2020-11-09 19:27:55 +0200

    first commit

---
+++ Killing Spider/assets/scripts/app.js
@@ -0,0 +1,40 @@
+let chosenMaxLife =100;
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE =14;
+const STRONG_ATTACK_VALUE =17;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackMonster(mode){
+    let maxDamage = 0;
+    if(mode === "ATTACK"){
+        maxDamage = ATTACK_VALUE;
+    } else if (mode === "STRONG_ATTACK"){
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage= dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+    const PlayerDamage =  dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= PlayerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth >0){
+        alert("You won!!")
+    } else if (currentPlayerHealth <=0 && currentPlayerHealth >0){
+        alert("You lost !!")
+    } else if (currentPlayerHealth <=0 && currentPlayerHealth <=0){
+        alert("its a tie !!")
+    }
+}
+
+function attackHandler() {
+    attackMonster("ATTACK");
+}
+
+function strongAttackGandler() {
+    attackMonster("STRONG_ATTACK");
+    
+}
+
+attackBtn.addEventListener("click",attackHandler);
+strongAttackBtn.addEventListener("click",strongAttackGandler);
