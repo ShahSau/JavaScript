216c407a4f3e5eb507f82b928cdfd5e4d4146108
ShahSau <shahriarksaurov@gmail.com>
2020-12-14 14:24:47 +0200

    updated dad joke project

---
+++ Mini Projects/Dad Jokes/script.js
@@ -0,0 +1,27 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn= document.getElementById('jokeBtn')
+
+
+jokeBtn.addEventListener('click',generateJoke)
+
+generateJoke()
+const config={
+    headers:{
+        'Accept': 'application/json'
+    }
+}
+async function generateJoke(){
+    const config={
+        headers:{
+            'Accept': 'application/json'
+        }
+    }
+    const res= await fetch('https://icanhazdadjoke.com',config)
+    const data= await res.json()
+
+    jokeEl.innerHTML=data.joke
+}
+
+
+
+
