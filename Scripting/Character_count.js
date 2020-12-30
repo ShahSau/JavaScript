3fc3ba21484797e1a423cf700835b3fe625e5a4b
ShahSau <shahriarksaurov@gmail.com>
2020-12-30 16:58:44 +0200

    scripting

---
+++ Scripting/Character_count.js
@@ -0,0 +1,16 @@
+function charCount(str){
+    let str2= str.replace(/ /g,'')
+    str2=str2.toLowerCase();
+    var ob = new Object();
+    for(let i=0; i< str2.length; i++){
+      if(str2[i] in ob){
+        ob[str2[i]]++
+      }else{
+          ob[str2[i]] = 1
+      }
+    }
+    return ob
+  }
+  
+  
+  charCount('Your phone number is 1234567')
